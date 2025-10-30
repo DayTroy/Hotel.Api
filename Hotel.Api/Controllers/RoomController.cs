541f5bc70deba57949b77a96249acef69583a089
shadowyguy5@gmail.com <shadowyguy5@gmail.com>
2025-10-31 00:44:48 +0300

    Added some NoWarns

--- Hotel.Api/Controllers/RoomController.cs
+++ Hotel.Api/Controllers/RoomController.cs
@@ -2,19 +2,29 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Hotel.Api.Controllers
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RoomController"/> class.
+    /// </summary>
     [ApiController]
     [Route("api/[controller]")]
     public class RoomController : ControllerBase
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RoomController"/> class.
+        /// </summary>
         public RoomController()
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RoomController"/> class.
+        /// </summary>
+        /// <returns>IActionResult.</returns>
         [HttpGet("types")]
         public IActionResult GetRoomTypes()
         {
             var roomTypes = new[] { "Single", "Double", "Suite", "Deluxe" };
-            return Ok(roomTypes);
+            return this.Ok(roomTypes);
         }
     }
 }
--- Hotel.Api/obj/Debug/net9.0/Hotel.Api.AssemblyInfo.cs
+++ Hotel.Api/obj/Debug/net9.0/Hotel.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hotel.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38bcda3447fd3dfae90fe2bb3fb72c87706c39e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hotel.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hotel.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
